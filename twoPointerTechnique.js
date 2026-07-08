af18ee2c9f8e32828bfba3da9c1f73ed73c0b172
nusratjahan7 <nusratjahan77222@gmail.com>
2026-07-08 12:23:42 +0600

    twoPointerTechnique solved

---
+++ twoPointerTechnique.js
@@ -0,0 +1,21 @@
+
+const reverseString = function (s) {
+    let left = 0;
+    let right = s.length - 1;
+    while (left < right) {
+        let temp = s[left];
+        s[left] = s[right];
+        s[right] = temp;
+
+        left++;
+        right--;
+    }
+    return s;
+}
+
+const s = ["a", "b", "c", "d"];
+const result = reverseString(s);
+
+console.log("array", result);
+
+// Output = 
